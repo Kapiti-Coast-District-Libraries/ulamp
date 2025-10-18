aa7ff11c82bb993cdfe3edd5241f24c16cc53ea8
ClarkWilliamsIE <clark@inertiaed.org>
2025-10-19 07:19:47 +1300

    Create main-admin.jsx

---
+++ src/admin/main-admin.jsx
@@ -0,0 +1,7 @@
+// src/admin/main-admin.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import AdminPage from "./AdminPage.jsx";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<AdminPage />);
