68e886c7db88b475a28b5ac29a04178dae6bc38d
ClarkWilliamsIE <clark@inertiaed.org>
2025-11-26 20:07:38 +1300

    Update index.js

--- src/packs/index.js
+++ src/packs/index.js
@@ -1,18 +1,20 @@
 // src/packs/index.js
- import shadePack from "./shadePack.js";
+import shadePack from "./shadePack.js";
 import superellipsePack from "./superellipsePack.js";
 import sockBallsPack from "./sockBallsPack.js";
-import lowPolyPack from "./lowPolyPack.js"
+import lowPolyPack from "./lowPolyPack.js";
 import wavySlatsPack from "./wavySlatsPack.js";
 import rippleSpiralPack from "./rippleSpiralPack.js";
 import meltSlumpPack from "./meltSlumpPack.js";
+import rosePack from "./rosePack.js"; // <--- Add this
 
- export const packs = {
-   shade: shadePack,
+export const packs = {
+  shade: shadePack,
   superellipse: superellipsePack,
   sock: sockBallsPack,
-    lowpoly: lowPolyPack,
-      wavyslats: wavySlatsPack,
-    ripplespiral: rippleSpiralPack,
-     meltslump: meltSlumpPack,
- };
+  lowpoly: lowPolyPack,
+  wavyslats: wavySlatsPack,
+  ripplespiral: rippleSpiralPack,
+  meltslump: meltSlumpPack,
+  rose: rosePack, // <--- And this
+};
